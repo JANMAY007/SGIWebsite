ef387df4fb5e638186d706b60d14966d5bb2f6d7
JANMAY007 <jrbhatt18p10@gmail.com>
2024-06-05 09:51:25 +0530

    Changed production settings for pythonanywhere

--- SGIWebsite/settings.py
+++ SGIWebsite/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_URL = '/static/'  # Ensure this starts and ends with a slash
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Note the tuple
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure this ends without a slash
 
 
 # Login Required redirect url
--- SGIWebsite/urls.py
+++ SGIWebsite/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Corrugation.urls')),
     path('offline/', TemplateView.as_view(template_name='offline.html'), name='offline'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
